7d10d58dfddeb3ccbbce9f7efe288a4c8d50811f
Sorawit_Sangboondee <142560602+lSoLoVEl@users.noreply.github.com>
2023-11-03 12:28:51 +0700

    Add files via upload

--- python_project09/test04.py
+++ python_project09/test04.py
@@ -1,2 +1,41 @@
-#คุณสมบัติเด่น OOP: Inheritanec
-#คุณสมบัติเด่น OOP: Polymorphism
+#คุณสมบัติเด่น OOP: Inheritanec สืบทอด
+#คือ คลาสหนึ่สิบทองอีกคลาสหนิ่ง (มีแม่ super/มีลูก sub)
+#พ่อเป็นแม่ลูกกันแม่มีอะไรลูกมีด้วย
+class TestA:
+    data1=10
+    data2=20
+
+    def showSAU():
+        print('Hi.....SAU')
+
+class TestB(TestA):
+    data3 = 30
+
+    def showWOW():
+        print('wow wow wow')
+class TestC(TestA):
+    dara4=40
+
+class TestD(TestB):
+    data5 = 50
+
+ob1 = TestA()
+ob2 = TestB()
+ob3 = TestD()
+
+
+
+
+
+
+
+#คุณสมบัติเด่น OOP: Polymorphismพ้องรูป
+# คือ พฤติกรรมเดียวกัน แต่วิธีการต่างกัน (ต้องเป็นแม่ลูกกัน )
+class Test2(TestA):
+    data6=60
+
+    #Polymorphism: Overding Method
+    def showSAU():
+        print('Hellp........SAU')
+
+ob4 = TestZ()
---
+++ python_project09/test05.py
@@ -0,0 +1,10 @@
+# module คือกลุ่มของ funtion , varible, contance,class
+#ที่เกี่ยวข้องกันเเละรวมอนู่ใน ไฟล์เดียวกัน เพื่อให้สามารถนำไปเรียกใช้งานที่ไหนก็ได้ โปรเจ็กต์ไหนก็ได้
+# ดั้งนั้น module ก็จะอยู่ในรูปของ file โดยชื่อ module ก็คือชื่อไฟล์
+
+def showHi( ) :
+    print('Hi.....')
+def showHey():
+    print('Hey.....')
+
+wow = 'Wow wow wow'
