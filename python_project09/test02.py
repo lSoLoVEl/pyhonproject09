d7e5e7dd3aa5f0af5f2abdcd828f153b0155bd72
Sorawit_Sangboondee <142560602+lSoLoVEl@users.noreply.github.com>
2023-11-03 12:27:46 +0700

    Add files via upload

---
+++ python_project09/test02.py
@@ -0,0 +1,27 @@
+class IoTThailand :
+    #data
+    wow = 100
+    wea = ''
+
+    #construtor ไม่ใช่member แต่จะทำงานทุกครั้งที่มีการสร้าง object
+    def __init__(self,woo,wee,wea):
+        self.woo = woo
+        self.wee = wee
+        self.wea = wea
+
+
+    #method
+    def showData(self):
+        print(self.wow * 20)
+
+    #destructor
+    def __del__(self):
+        print('good morning')
+
+ob1 = IoTThailand(10,20,30)
+ob2 = IoTThailand(10,20,30)
+ob3 = IoTThailand(5,20,10)
+ob4 = IoTThailand(10,20,10)
+
+print(ob1.wea + ob2.wea)
+ob3.showData()
